bc679df2a9aa1c0f6a0e0e4f0b53202fa621cd10
Malka Bachrach <malka.bachrach@ti.htc.edu>
2018-02-06 15:20:05 -0600

    second commit

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,14 +1,25 @@
 package hw;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
-  public static void main(final String[] args) {
-    System.out.println("args = " + Arrays.asList(args));
-    final HelloWorld instance = new HelloWorld();
-    System.out.println(instance.getMessage());
-    System.out.println(instance.getYear());
-    System.out.println("bye for now");
+  public static void main(String[]args)
+  {
+    System.out.println("Please enter an integer");
+    Scanner keyboard=new Scanner(System.in);
+    int number=keyboard.nextInt();
+    for(int i=1;i<=number;i++)
+    {
+      if ((i%3==0)&&(i%5==0))
+        System.out.println("Fizz Buzz");
+      else if(i%3==0)
+        System.out.println("Fizz");
+      else if(i%5==0)
+        System.out.println("Buzz");
+      else
+        System.out.println(i);
+    }
   }
 }
